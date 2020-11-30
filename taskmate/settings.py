c0c166b23003d155f79eea0ba3c01a80439a3c0a
mounvip1525 <mounvip1525@gmail.com>
2020-11-30 12:43:02 +0530

    authentication and login views created

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todolist_app',
+    'users_app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+CRISPY_TEMPLATE_PACK='bootstrap4' #by-default cripsy_forms supoort only bootsrap2
